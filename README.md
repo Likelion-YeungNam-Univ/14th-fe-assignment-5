c6a819afb30ed7622325e7d6dcdd314054f9d54c
Park Darae <ret7258@naver.com>
2026-04-30 19:05:14 +0900

    feat: 기본 틀 구성 및 레퍼런스 이미지 추가

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 14th-fe-assignment-5
 영남대 멋쟁이사자처럼 14기 프론트엔드 트랙 5차시 과제
+![레퍼런스용 웹페이지](./images/reference_webpage.png)
---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      {/* <div className="w-30 h-20 text-3xl text-center text-orange-500 bg-blue-800">14th YU LikeLion</div>
+      <p className="text-blue-600/100 dark:text-sky-400/100">14th YU LikeLion</p>
+      <p className="text-blue-600/75 dark:text-sky-400/75">14th YU LikeLion</p>
+      <p className="text-blue-600/50 dark:text-sky-400/50">14th YU LikeLion</p> */}
+      {/* <p className="text-blue-600/25 dark:text-sky-400/25">14th YU LikeLion</p>
+      <div className="p-4 bg-blue-100 text-center">Padding all: p-4</div>
+      <div className="pt-2 pr-3 pb-1 pl-5 bg-green-100 text-center">Padding separate: pt-2 pr-3 pb-1 pl-5</div>
+      <div className="px-2 py-3 bg-yellow-100 text-center">Padding axes: px-2 py-3</div>
+      <div className="m-[16px] bg-red-100 border-2 border-red-500 text-center">
+        Margin all
+      </div>
+      <div className="mt-[8px] mr-[12px] mb-[4px] ml-[20px] bg-purple-100 border-2 border-purple-500 text-center">
+        Margin separate
+      </div>
+      <div className="mx-[8px] my-[12px] bg-pink-100 border-2 border-pink-500 text-center">
+        Margin axes
+      </div>
+      <div className="flex space-x-[16px] bg-gray-100 p-[16px] border-2 border-gray-500">
+        <div className="bg-blue-500 text-white p-[8px] border">Item 1</div>
+        <div className="bg-blue-500 text-white p-[8px] border">Item 2</div>
+        <div className="bg-blue-500 text-white p-[8px] border">Item 3</div>
+      </div>
+      <div className="flex flex-col space-y-[10px] bg-gray-200 p-[6px] border-2 border-gray-600">
+        <div className="bg-green-500 text-white p-[8px] border">Item A</div>
+        <div className="bg-green-500 text-white p-[8px] border">Item B</div>
+        <div className="bg-green-500 text-white p-[8px] border">Item C</div>
+      </div> */}
+      <div className="max-w-screen-full h-20 text-3xl text-center text-orange-500 bg-blue-800 
+                md:bg-green-500 lg:bg-red-500 xl:text-white">
+        14th YU LikeLion
+      </div>
+    </div>
+  )
+
+}
+
+export default App;
