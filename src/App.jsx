ed9f18d1c9a9f627eea23bdc86ad7bf587aae577
knayoung0 <kzeron09@naver.com>
2026-05-03 17:11:02 +0900

    14th-5-최종

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Story from './Story'
+import Post from './Post'
+import Head from './Head'
+import Message from './Message'
+
+
+const App = () => {
+  return (
+    <>
+      <Head/>
+      <Story/>
+      <Post/>
+      <Message/>
+    </>
+  )
+}
+
+export default App
